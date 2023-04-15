6f0ee6c74de13201f7304c4484e54935109e282c
Aypisam <the.aypisam.fpv@gmail.com>
2023-04-15 03:36:49 +0200

    ...

--- natas11/coockie_decrypt.py
+++ natas11/coockie_decrypt.py
@@ -1,13 +1,25 @@
+from email.mime import base
 import json
+import base64
 
-def xor_encrypt(inp):
-    key = '<censored>'
-    text = inp.encode('utf-8')
-    out_text = b''
+encrypted_cookie = 'MGw7JCQ5OC04PT8jOSpqdmkgJ25nbCorKCEkIzlscm5oKC4qLX54bjY='
+
+
+default_cookie = {
+    "showpassword": "no",
+    "bgcolor": "#ffffff"
+}
+
+cookies = {"data": None}
+
+def xor_encrypt(inp:str):
+    key = 'hello world'
+    text = inp
+    out_text = ''
 
     # Iterate through each character
     for i in range(len(text)):
-        out_text += bytes([text[i] ^ key[i % len(key)]])
+        out_text += str(str(text[i]) ^ str(key[i % len(key)]))
 
     return out_text
 
@@ -23,8 +35,11 @@ def xor_decrypt(inp):
 
     return out_text.decode('utf-8')
 
-"""decode json"""
-def decode_json(inp):
-    return json.loads(xor_decrypt(inp))
 
-print(decode_json('MGw7JCQ5OC04PT8jOSpqdmkgJ25nbCorKCEkIzlscm5oKC4qLX54bjY='))
+def save_data(data): 
+    return base64.b64encode(xor_encrypt(json.dumps(data).encode('utf-8'))).decode('utf-8')
+
+
+# cookies["data"] = save_data(default_cookie)
+
+print(json.dumps(default_cookie).encode('utf-8'))
